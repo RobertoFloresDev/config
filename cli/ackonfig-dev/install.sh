214499e8051f83f8b1ee36388f91403cfa1b6c2f
Roberto Flores <roberto.flores@ackts.com>
2025-06-10 03:43:58 +0000

    fix correct clean before reinstall ackonfig-dev cli

--- cli/ackonfig-dev/install.sh
+++ cli/ackonfig-dev/install.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y curl
 mkdir -p ~/.ackonfig-dev
+rm -rf ~/.ackonfig-dev/*
 mkdir -p ~/.ackonfig-dev/bash
 curl -sSL https://raw.githubusercontent.com/RobertoFloresDev/config/refs/heads/main/cli/ackonfig-dev/ackonfig-dev.sh -o ~/.ackonfig-dev/ackonfig-dev.sh
 curl -sSL https://raw.githubusercontent.com/RobertoFloresDev/config/refs/heads/main/cli/ackonfig-dev/bash/.bashrc -o ~/.ackonfig-dev/bash/.bashrc
