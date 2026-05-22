1999882ca4a4ecac06a1572b9471ac6e2fd82fae
Roberto Flores <97043148+RobertoFloresDev@users.noreply.github.com>
2026-05-22 12:03:48 -0600

    add GL.iNet configuration

---
+++ SBC/GL.iNet/BE3600/config.sh
@@ -0,0 +1,33 @@
+# Config BE3600
+
+# Verify Upgrade
+- Set (SYSTEM > Upgrade)
+
+# Set Password
+- Set (SYSTEM > Admin Password)
+
+# Set Network
+- Set (NETWORK > LAN)
+- Router IP Address as 10.0.0.1
+- Set (NETWORK > Guest Network)
+- Gateway as 10.0.1.1
+
+# Wreless
+- Set (WIRELESS)
+- NO MLO Wi-Fi
+- Enable 5/2.5 Ghz Regular/Guest Wi-Fi
+- Same SSID for 5/2.5 Ghz Regular (Visibility Hidden)
+- Same SSID for 5/2.5 Ghz Guest
+
+# Set Display
+- Set (SYSTEM > Display Management)
+- Lock Screen as Wallpaper Only
+- Auto Lock 1 Minute
+- Enalbe Screen Passcode
+- Function Management
+  - Overview
+  - 2.4 Hz Guest Wi-Fi
+  - 5 Hz Guest Wi-Fi
+  - VPN Dashboard
+  - 2.4 GHz Wi-Fi
+  - 5 GHz Wi-Fi
