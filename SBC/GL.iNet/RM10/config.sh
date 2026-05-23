051e17c053627b5238538ac07f4b3a8ebd80b871
Roberto Flores <97043148+RobertoFloresDev@users.noreply.github.com>
2026-05-22 19:12:24 -0600

    configure GL.iNet GL-RM10

---
+++ SBC/GL.iNet/RM10/config.sh
@@ -0,0 +1,14 @@
+# Config RM10
+
+
+# Set Color Mode
+- Set (Settings > System > Color Mode) to Dark
+
+# Set Timezone
+- Set (Settings > System > Timezone) to America/Mexico_City
+
+# Set Device Identity
+- Set (Settings > System > Device Identity) to 1080P ASUS + Logitech Inc Keyboard
+
+# Set Tailscale
+- Set (App Center) Tailscale ON and Bind Device
