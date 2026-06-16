d46db79ba88227cff6e1cc1b38c3c9dcbbb42e00
Roberto Flores <97043148+RobertoFloresDev@users.noreply.github.com>
2026-06-16 17:18:32 -0600

    update compose configurations

--- compose/create.sh
+++ compose/create.sh
@@ -3,9 +3,11 @@
 # - Create stacks folders
 sudo mkdir -p /opt/stacks/compose
 sudo mkdir -p /opt/stacks/data
+sudo mkdir -p /opt/stacks/track
 sudo mkdir -p /opt/stacks/discard
 sudo chown -R $USER:$USER /opt/stacks/compose
 sudo chown -R $USER:$USER /opt/stacks/data
+sudo chown -R $USER:$USER /opt/stacks/track
 sudo chown -R $USER:$USER /opt/stacks/discard
 
 # - Create proxy network
